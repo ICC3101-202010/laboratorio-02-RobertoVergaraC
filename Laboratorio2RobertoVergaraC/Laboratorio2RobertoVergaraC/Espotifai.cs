8fce18b6ba7d4c446d6f25e2b78ad488604918be
RobertoVergaraC <ravergara@miuandes.com>
2020-03-25 21:10:41 -0300

    Incremento 1: Class Espotifai ready

--- Laboratorio2RobertoVergaraC/Laboratorio2RobertoVergaraC/Espotifai.cs
+++ Laboratorio2RobertoVergaraC/Laboratorio2RobertoVergaraC/Espotifai.cs
@@ -8,5 +8,37 @@ namespace Laboratorio2RobertoVergaraC
 {
     class Espotifai
     {
+        List<Cancion> songs = new List<Cancion>();
+
+        public Espotifai()
+        {
+           
+        }
+        
+        public bool AgregarCancion(Cancion cancion)
+        {
+            for (int i = 0; i < songs.Count; i++)
+            {
+                if (cancion == songs[i])
+                    {
+                        songs.Add(cancion);
+                        return true; 
+                    }
+                else 
+                    {
+                        continue;
+                    }
+            }
+            return false;
+        }
+
+        public void VerCanciones()
+        {
+            Console.WriteLine("La lista de canciones es:");
+            for (int i = 0; i < songs.Count; i++)
+            {
+                Console.WriteLine(songs[i]);
+            }
+        }
     }
 }
